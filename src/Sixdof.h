7edc75b828681ea82a28369a41fb588ea90d3373
MarcoZXCAS <139630422+MarcoZXCAS@users.noreply.github.com>
2025-03-05 11:51:14 +0100

    Update Sixdof.h

--- src/Sixdof.h
+++ src/Sixdof.h
@@ -5,17 +5,22 @@
 #include <Servo.h>
 
 #define NUM_SERVI 6
+#define PINZA_APERTA  45
+#define PINZA_CHIUSA  100
+#define POS_DEFAULT   90
 
 class SixDOF {
 public:
     SixDOF(int Base, int Spalla, int Gomito, int Polso_1, int Polso_2, int Pinza);
     
-    void chiudi_pinza();  // Chiude la pinza (imposta un angolo specifico)
-    void apri_pinza();    // Apre la pinza (imposta un altro angolo)
+    void chiudi_pinza();  // Chiude la pinza
+    void apri_pinza();    // Apre la pinza
+    void reset_all();     // Imposta tutto a 90° (tranne la pinza)
+    void muovi_servo(int indice, int angolo); // Muove un singolo servo a un angolo specifico
 
 private:
-    Servo _servi[NUM_SERVI]; // Array di 6 oggetti Servo
-    int _pin[NUM_SERVI];     // Pin collegati ai servi
+    Servo _servi[NUM_SERVI]; // Array di oggetti Servo
+    int _pin[NUM_SERVI];     // Pin collegati ai servomotori
 };
 
 #endif
