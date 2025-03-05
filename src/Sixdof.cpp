9ff684bdd5e91e81beb2e85578d4ee007b960488
MarcoZXCAS <139630422+MarcoZXCAS@users.noreply.github.com>
2025-03-05 11:50:53 +0100

    Update Sixdof.cpp

--- src/Sixdof.cpp
+++ src/Sixdof.cpp
@@ -9,14 +9,37 @@ SixDOF::SixDOF(int Base, int Spalla, int Gomito, int Polso_1, int Polso_2, int P
     _pin[5] = Pinza;
 
     for (int i = 0; i < NUM_SERVI; i++) {
-        _servi[i].attach(_pin[i]);  // Collega i servi ai pin specificati
+        _servi[i].attach(_pin[i]);
     }
 }
 
 void SixDOF::apri_pinza() {
-    _servi[5].write(45);  // Muove il servo della pinza a 45°
+    _servi[5].write(PINZA_APERTA);
 }
 
 void SixDOF::chiudi_pinza() {
-    _servi[5].write(100); // Muove il servo della pinza a 100°
+    _servi[5].write(PINZA_CHIUSA);
 }
+
+void SixDOF::reset_all() {
+    for (int i = 0; i < (NUM_SERVI - 1); i++) {
+        _servi[i].write(POS_DEFAULT);
+        }
+    chiudi_pinza();
+}
+
+void SixDOF::muovi_servo(int indice, int angolo) {
+    if (indice >= 0 && indice < NUM_SERVI && angolo >= 0 && angolo <= 180) {
+        _servi[indice].write(angolo);
+        return;
+    }
+
+    if (indice < 0 || indice >= NUM_SERVI) {
+        Serial.println("Errore: Indice servo non valido");
+    }
+
+    if (angolo < 0 || angolo > 180) {
+        Serial.println("Errore: Angolo servo non valido");
+    }
+}
+
